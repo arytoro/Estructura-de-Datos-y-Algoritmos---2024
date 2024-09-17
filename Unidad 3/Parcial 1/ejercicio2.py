317fe612489f29d1b1cf54e3d4f14728660420fa
arytoro <166941413+arytoro@users.noreply.github.com>
2024-09-17 19:38:42 -0300

    Add files via upload

---
+++ Unidad 3/Parcial 1/ejercicio2.py
@@ -0,0 +1,16 @@
+"""Ejercicio 2 / Unidad 3 - Ary Toro"""
+from pila_secuencial import PilaSecuencial
+
+if __name__=="__main__":
+    p=PilaSecuencial()
+    decimal=int(input("Ingresa el numero decimal: "))
+    aux=decimal
+    while aux>=1:
+        modulo=aux%2
+        aux=aux//2
+        p.insertar(modulo)
+    print("Error. Se esperaba un entero")
+        
+    print("En binario es: ",end=" ")
+    while p.vacia() is False:
+        print(p.suprimir(),end="")
